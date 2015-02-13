fdca41c561b465fcc03efbc1275966e3f8152d3b
Christian Avemark <christian@avemark.se>
2015-02-13 16:47:47 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToJsonTest::Application.config.secret_key_base = 'f640f4d187286c1cbfbf6330037c0b4022de2ebfd5bd08df3f73750f9ab1a73cf990ca0fd9bb8a62c3dc19c161640bb97dabb4a2bec8cec80f7abfa4597fa34f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ToJsonTest::Application.config.session_store :cookie_store, key: '_to_json_test_session'
